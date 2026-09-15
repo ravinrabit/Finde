a921a90c88ff93877e6027c6d8030893e028fd1d
Nikolas Neres Moreira <nikolas64885386@edu.df.senac.br>
2026-09-15 14:54:44 -0300

    agora vai

--- setup/settings/base.py
+++ setup/settings/base.py
@@ -13,7 +13,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = False
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1,10.0.2.2", cast=Csv())
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1,10.0.2.2",
+    cast=Csv(),
+)
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="", cast=Csv())
 
 SITE_NOME = config("SITE_NOME", default="Finde")
